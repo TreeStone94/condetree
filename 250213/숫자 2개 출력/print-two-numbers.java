9f974526734c13ccf2df3e68d463d8376d1f9fc4
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-02-13 12:45:24 +0900

    Title: 숫자 2개 출력, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250213/숫자 2개 출력/print-two-numbers.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int x = 3;
+        int y = 5;
+        System.out.print(x + " " + y);
+    }
+}
