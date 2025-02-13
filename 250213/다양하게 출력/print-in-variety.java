369bfd28d85155b56d2a16203c7eaaa029081293
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-02-13 12:47:24 +0900

    Title: 다양하게 출력, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250213/다양하게 출력/print-in-variety.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        double d = 3.1415926535;
+        int x = 365;
+        System.out.println("Total days in Year");
+        System.out.println(x);
+        System.out.println("Circumference rate");
+        System.out.println(d);
+
+    }
+}
